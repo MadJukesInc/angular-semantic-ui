bdbc17fb08de3c50521ec48eb11b5ebcaaff030c
kpineau <kyle.pineau@deltaware.com>
2016-01-25 08:58:29 -0400

    feat(modal): cleanup modal to allow settings, and rebuild dist

--- dist/angular-semantic-ui.js
+++ dist/angular-semantic-ui.js
@@ -382,38 +382,50 @@ angular.module('angularify.semantic.dropdown', [])
 
 angular.module('angularify.semantic.modal', [])
 
-    .directive('modal', function () {
-        return {
-            restrict: 'E',
-            replace: true,
-            transclude: true,
-            require: 'ngModel',
-            scope: {
-                visibility : "=ngModel",
-                onApprove : "=?",
-                onDeny : "=?"
-            },
-            template: '<div class="ui modal" ng-transclude></div>',
-            link: function (scope, element, attrs, ngModel) {
-                element.modal({
-                    onHide: function () {
-                        ngModel.$setViewValue(false);
-                    },
-                    onApprove: function() {
-                        if(scope.onApprove) return scope.onApprove();
-                    },
-                    onDeny: function() {
-                        if(scope.onDeny) return scope.onDeny();
-                    }
-                });
-                scope.$watch(function () {
-                    return ngModel.$modelValue;
-                }, function (modelValue){
-                    element.modal(modelValue ? 'show' : 'hide');
-                });
-            }
+  .directive('modal', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      transclude: true,
+      require: 'ngModel',
+      scope: {
+        visibility: "=ngModel",
+        onApprove: "=?",
+        onDeny: "=?",
+        settings: "=?"
+      },
+      template: '<div class="ui modal" ng-transclude></div>',
+      link: function(scope, element, attrs, ngModel) {
+        var settings = {};
+
+        if (scope.settings) {
+          settings = scope.settings;
         }
-    });
+
+        settings.onHide = function() {
+          ngModel.$setViewValue(false);
+        };
+        settings.onApprove = function() {
+          if (scope.onApprove) return scope.onApprove();
+        };
+        settings.onDeny = function() {
+          if (scope.onDeny) return scope.onDeny();
+        };
+
+        //settings.observeChanges = true;
+
+        element.modal(settings);
+
+        scope.$watch(function() {
+          return ngModel.$modelValue;
+        }, function(modelValue) {
+          setTimeout(function() {
+            element.modal(modelValue ? 'show' : 'hide');
+          }, 0);
+        });
+      }
+    }
+  });
 'use strict';
 
 angular.module('angularify.semantic.popup', [])
--- src/modal/modal.js
+++ src/modal/modal.js
@@ -2,35 +2,45 @@
 
 angular.module('angularify.semantic.modal', [])
 
-    .directive('modal', function () {
-        return {
-            restrict: 'E',
-            replace: true,
-            transclude: true,
-            require: 'ngModel',
-            scope: {
-                visibility : "=ngModel",
-                onApprove : "=?",
-                onDeny : "=?"
-            },
-            template: '<div class="ui modal" ng-transclude></div>',
-            link: function (scope, element, attrs, ngModel) {
-                element.modal({
-                    onHide: function () {
-                        ngModel.$setViewValue(false);
-                    },
-                    onApprove: function() {
-                        if(scope.onApprove) return scope.onApprove();
-                    },
-                    onDeny: function() {
-                        if(scope.onDeny) return scope.onDeny();
-                    }
-                });
-                scope.$watch(function () {
-                    return ngModel.$modelValue;
-                }, function (modelValue){
-                    element.modal(modelValue ? 'show' : 'hide');
-                });
-            }
+  .directive('modal', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      transclude: true,
+      require: 'ngModel',
+      scope: {
+        visibility: "=ngModel",
+        onApprove: "=?",
+        onDeny: "=?",
+        settings: "=?"
+      },
+      template: '<div class="ui modal" ng-transclude></div>',
+      link: function(scope, element, attrs, ngModel) {
+        var settings = {};
+
+        if (scope.settings) {
+          settings = scope.settings;
         }
-    });
+
+        settings.onHide = function() {
+          ngModel.$setViewValue(false);
+        };
+        settings.onApprove = function() {
+          if (scope.onApprove) return scope.onApprove();
+        };
+        settings.onDeny = function() {
+          if (scope.onDeny) return scope.onDeny();
+        };
+
+        element.modal(settings);
+
+        scope.$watch(function() {
+          return ngModel.$modelValue;
+        }, function(modelValue) {
+          setTimeout(function() {
+            element.modal(modelValue ? 'show' : 'hide');
+          }, 0);
+        });
+      }
+    }
+  });
